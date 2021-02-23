a3c39459fb7a083d97b7a4ee23ba0f2acd435e8b
Vikash Kumar <35370115+vikash-kumar01@users.noreply.github.com>
2021-02-23 13:02:50 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,20 @@
+variable "rgName" {
+    default = "M1057980RG"
+}
+variable "location" {
+    default = "eastus" 
+}
+
+variable "vnetName" {
+    default = "vNet" 
+}
+variable "subnetName" {
+    default = "internal" 
+}
+variable "nicName" {
+    default = "nic" 
+}
+
+variable "vmName" {
+    default = "VM" 
+}
